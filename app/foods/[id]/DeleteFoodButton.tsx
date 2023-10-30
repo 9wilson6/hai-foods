3d09d02dc37e28728cad97a2621cb90df6bd0a76
Wilson Gatheru <gatheruwilson@gmail.com>
2023-10-30 19:35:59 +0300

    Delete Confirmation Dialog

--- app/foods/[id]/DeleteFoodButton.tsx
+++ app/foods/[id]/DeleteFoodButton.tsx
@@ -1,4 +1,5 @@
-import { Button } from "@radix-ui/themes";
+"use client";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import React from "react";
 
 interface Props {
@@ -6,5 +7,28 @@ interface Props {
 }
 
 export default function DeleteFoodButton({ foodId }: Props) {
-  return <Button color="red">Delete Food</Button>;
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete Food</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue? This action cannot be
+          undone
+        </AlertDialog.Description>
+        <Flex mt="4" gap="3">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">Delete Food</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
 }
