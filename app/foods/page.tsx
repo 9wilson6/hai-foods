a0f26eb83c67ccced6147994302835435582d738
Wilson Gatheru <gatheruwilson@gmail.com>
2023-10-30 09:01:33 +0300

    radix-themes implementation

--- app/foods/page.tsx
+++ app/foods/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from "@radix-ui/themes";
 import React from "react";
 
 export default function Foods() {
-  return <div>Foods</div>;
+  return (
+    <div>
+      <Button>New Food</Button>
+    </div>
+  );
 }
