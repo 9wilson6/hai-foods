ba51fde4b4e9d39ebab855670d70d3e9912c9d61
Wilson Gatheru <gatheruwilson@gmail.com>
2023-10-30 09:45:26 +0300

    New Food

--- app/foods/new/page.tsx
+++ app/foods/new/page.tsx
@@ -1,16 +1,39 @@
 "use client";
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, TextField } from "@radix-ui/themes";
+import { useForm, Controller } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface NewFoodPageProps {
+  title: string;
+  description: string;
+}
 
 export default function NewFoodPage() {
+  const { register, control, handleSubmit } = useForm<NewFoodPageProps>();
+
+  const router = useRouter();
   return (
-    <div className="max-w-xl space-y-3">
+    <form
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/foods", data);
+        router.push("/foods");
+      })}
+      className="max-w-xl space-y-3"
+    >
       <TextField.Root>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
-      <SimpleMDE placeholder="Description" />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
       <Button>Submit New Food</Button>
-    </div>
+    </form>
   );
 }
