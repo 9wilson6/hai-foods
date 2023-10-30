0382b54fbd3f3c92b43ebea1974252ae35f4893a
Wilson Gatheru <gatheruwilson@gmail.com>
2023-10-30 08:10:58 +0300

    navlinks active styling

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,9 +1,14 @@
+"use client";
 import { navItems } from "@/data/siteData";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { MdFoodBank } from "react-icons/md";
+import classNames from "classnames";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/" className="flex gap-2 items-center">
@@ -18,7 +23,11 @@ export default function Navbar() {
               <li key={id}>
                 <Link
                   href={href}
-                  className="text-zinc-500 hover:text-zinc-800 transition-colors"
+                  className={classNames({
+                    "text-zinc-500": href === pathname,
+                    "text-zinc-900": href !== pathname,
+                    "hover:text-zinc-800 transition-colors": true,
+                  })}
                 >
                   {title}
                 </Link>
