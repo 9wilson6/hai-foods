9c286a4e3bd48ba87a4a075ab1c4fb0af2e293f8
Wilson Gatheru <gatheruwilson@gmail.com>
2023-10-30 08:55:47 +0300

    api: create foods endpoint

---
+++ app/api/foods/route.ts
@@ -0,0 +1,23 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const createFoodSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  const validation = createFoodSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
+  const newFood = await prisma.food.create({
+    data: { title: body.title, description: body.description },
+  });
+
+  return NextResponse.json(newFood, { status: 201 });
+}
