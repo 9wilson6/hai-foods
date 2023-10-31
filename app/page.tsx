0d346735639cb6bc5b046035ce09b3c79f14ee97
Wilson Gatheru <gatheruwilson@gmail.com>
2023-10-31 11:29:06 +0300

    Pagination Layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import Pagination from "@/components/Pagination";
+import { Heading } from "@radix-ui/themes";
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Heading>Home</Heading>
+      <Pagination itemCount={100} pageSize={10} currentPage={2} />
+    </>
+  );
 }
