82b14bb9962073338bf83b0ab47c4cdb8b5f8582
Wilson Gatheru <gatheruwilson@gmail.com>
2023-10-31 11:40:39 +0300

    implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,20 @@ import Pagination from "@/components/Pagination";
 import { Heading } from "@radix-ui/themes";
 import React from "react";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
     <>
       <Heading>Home</Heading>
-      <Pagination itemCount={100} pageSize={10} currentPage={2} />
+      <Pagination
+        itemCount={100}
+        pageSize={10}
+        currentPage={parseInt(searchParams.page)}
+        // currentPage={2}
+      />
     </>
   );
 }
