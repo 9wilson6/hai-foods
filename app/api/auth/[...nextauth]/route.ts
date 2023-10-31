ca77a598793e364a2ab8df70a71683c8e6402972
Wilson Gatheru <gatheruwilson@gmail.com>
2023-10-31 06:54:55 +0300

    configure Github Provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,12 @@
 import NextAuth from "next-auth";
-
+import GithubProvider from "next-auth/providers/github";
 const handler = NextAuth({
-  providers: [],
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
 });
 
 export { handler as GET, handler as POST };
