053cce44206481483fbc3e2492d1a8aafec6579a
Wilson Gatheru <gatheruwilson@gmail.com>
2023-10-31 12:07:13 +0300

    Home section

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,12 @@
+import LatestFoods from "@/components/LatestFoods";
 import Pagination from "@/components/Pagination";
 import { Heading } from "@radix-ui/themes";
 import React from "react";
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
+export default function Home() {
   return (
     <>
-      <Heading>Home</Heading>
-      <Pagination
-        itemCount={100}
-        pageSize={10}
-        currentPage={parseInt(searchParams.page)}
-        // currentPage={2}
-      />
+      <LatestFoods />
     </>
   );
 }
---
+++ components/LatestFoods.tsx
@@ -0,0 +1,50 @@
+import prisma from "@/lib/prisma";
+import { Avatar, Card, Flex, Heading, Table } from "@radix-ui/themes";
+import Link from "next/link";
+import React from "react";
+import FoodStateBadge from "./FoodStatusBadge";
+
+export default async function LatestFoods() {
+  const foods = await prisma.food.findMany({
+    orderBy: { createdAt: "desc" },
+    take: 5,
+    include: {
+      assignedToUser: true,
+    },
+  });
+  return (
+    <Card>
+      <Heading size="4" mb="5">
+        Eat Organic Foods
+      </Heading>
+      <Table.Root>
+        <Table.Body>
+          {foods.map((food) => {
+            return (
+              <>
+                <Table.Row key={food.id}>
+                  <Table.Cell>
+                    <Flex justify="between">
+                      <Flex direction="column" align="start" gap="2">
+                        <Link href={`/foods/${food.id}`}>{food.title}</Link>
+                        <FoodStateBadge status={food.status} />
+                      </Flex>
+                      {food.assignedToUser && (
+                        <Avatar
+                          src={food.assignedToUser.image!}
+                          fallback="?"
+                          size="1"
+                          radius="full"
+                        />
+                      )}
+                    </Flex>
+                  </Table.Cell>
+                </Table.Row>
+              </>
+            );
+          })}
+        </Table.Body>
+      </Table.Root>
+    </Card>
+  );
+}
